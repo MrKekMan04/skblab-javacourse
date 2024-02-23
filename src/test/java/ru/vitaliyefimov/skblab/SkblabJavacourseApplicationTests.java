876b671a743b73a121f6271aec8b56f227194374
Vitaliy <vitaliy-efimov111@mail.ru>
2024-02-23 19:39:55 +0500

    Initial commit

---
+++ src/test/java/ru/vitaliyefimov/skblab/SkblabJavacourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vitaliyefimov.skblab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkblabJavacourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
