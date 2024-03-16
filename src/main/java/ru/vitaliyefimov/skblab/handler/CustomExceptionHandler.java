b1df47cd05d2aca16a5b068dc18dcb765393de11
Vitaliy <vitaliy-efimov111@mail.ru>
2024-03-16 15:28:29 +0500

    Updated handler with ApiErrorResponse

--- src/main/java/ru/vitaliyefimov/skblab/handler/CustomExceptionHandler.java
+++ src/main/java/ru/vitaliyefimov/skblab/handler/CustomExceptionHandler.java
@@ -3,13 +3,14 @@ package ru.vitaliyefimov.skblab.handler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.vitaliyefimov.skblab.entity.dto.ApiErrorResponse;
 import ru.vitaliyefimov.skblab.exception.CustomException;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<CustomException> customException(CustomException exception) {
+    public ResponseEntity<ApiErrorResponse> customException(CustomException exception) {
         return ResponseEntity.status(exception.getStatusCode())
-                .body(exception);
+                .body(new ApiErrorResponse(exception.getStatusCode().value(), exception.getReason()));
     }
 }
