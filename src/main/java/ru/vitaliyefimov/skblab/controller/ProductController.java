a42f26cb0a114eb0301351c607b23fc29da46bae
Vitaliy <vitaliy-efimov111@mail.ru>
2024-03-16 15:30:01 +0500

    Updated product controller with product service and replaced request mapping type

--- src/main/java/ru/vitaliyefimov/skblab/controller/ProductController.java
+++ src/main/java/ru/vitaliyefimov/skblab/controller/ProductController.java
@@ -1,17 +1,22 @@
 package ru.vitaliyefimov.skblab.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.vitaliyefimov.skblab.entity.dto.ProductRequest;
 import ru.vitaliyefimov.skblab.entity.dto.ProductResponse;
+import ru.vitaliyefimov.skblab.service.ProductService;
 
 @RestController
 @RequestMapping("/product")
+@RequiredArgsConstructor
 public class ProductController {
-    @GetMapping
+    private final ProductService productService;
+
+    @PostMapping
     public ProductResponse getProduct(@RequestBody ProductRequest request) {
-        return new ProductResponse(request.price(), new ProductResponse.ProductInfo(123L, request.info().date()));
+        return productService.addId(request);
     }
 }
